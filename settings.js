462928c0b59018ad54124bad31b80980b7ba1ca9
zalemjp <zalem.j.p@gmail.com>
2025-04-30 00:40:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'zalemjp', // Your name
+  ownerNumber: '237620615398', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
